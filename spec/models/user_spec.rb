7c0c5b67ab054d1b153fd6da0b2e1b07c0c1c520
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-01-23 15:22:25 -0600

    Reorganize user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,12 +14,15 @@ RSpec.describe User, type: :model do
       expect(invalid_user.save).to eq false
       expect(invalid_user2.save).to eq false
     end
+    
+    it { should validate_uniqueness_of(:email) }
+
+    it { should validate_presence_of(:email) }
   end 
 
-  it { should validate_presence_of(:email) }
   it { should validate_presence_of(:name) }
 
-  it { should validate_length_of(:password).is_at_least(6) }
+  it { should validate_presence_of(:password) }
 
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_length_of(:password).is_at_least(6) }
 end

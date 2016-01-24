66d1b445dcde6b87a3719d03c005bbabe4c7bb66
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-01-24 11:35:31 -0600

    Add skeleton for checkmark binding

--- app/assets/javascripts/answerForms.js
+++ app/assets/javascripts/answerForms.js
@@ -2,16 +2,24 @@ $(document).ready(function() {
   $('.answer-form').on("ajax:success", function(e, data, status, xhr) {
     clearInputs();
     if(xhr.status === 200) {
-      $("#questions-list").append(xhr.responseText)
+      $("#questions-list").append(xhr.responseText);
     } else if(xhr.status === 203) {
-      $("#questions-error").append(xhr.responseText)
+      $("#questions-error").append(xhr.responseText);
     }
   }).on("ajax:error", function(e, data, status, xhr) {
-    console.log('There was an error')
+    console.log('There was an error');
+  });
+
+  $(".answer-checkbox").on('click', function(e) {
+    // toggleCheckbox()
   });
 });
 
 var clearInputs = function() {
   $('.answer-form').trigger("reset");
-  $("#questions-error").empty()
+  $("#questions-error").empty();
 };
+
+var toggleCheckbox = function() {
+  $(".answer-correctness-indicator").toggleClass("hidden");
+}

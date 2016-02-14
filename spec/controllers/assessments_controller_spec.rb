33ee20d5cbf865722d3a8d55ca8410c71bd2a494
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-02-14 17:17:30 -0600

    Test assessment creation

--- spec/controllers/assessments_controller_spec.rb
+++ spec/controllers/assessments_controller_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe AssessmentsController, type: :controller do
+  describe 'POST create' do
+    let(:creator) { FactoryGirl.create(:user) }
+    let(:assessment) { Assessment.find_by(title: "Valid Assessment") }
+    let(:assessment_params) do
+      { assessment: { creator: creator, title: "Valid Assessment" } }
+    end 
+    let(:invalid_params) do
+      { assessment: { creator: creator, title: "" } }
+    end 
 
+
+    before do
+      allow(controller).to receive(:current_user) { creator }
+    end
+
+    describe 'valid' do
+      before do
+        request = post :create, assessment_params
+      end
+
+      it 'redirects to add questions' do
+        expect(request).to redirect_to assessment_add_questions_path(assessment)
+      end
+
+      it 'assigns to right attributes' do
+        expect(assessment.title).to eq 'Valid Assessment'
+        expect(assessment.creator).to eq creator
+      end
+
+      it { should_not set_flash }
+    end
+
+    describe 'invalid' do
+      before do
+        request = post :create, invalid_params
+      end
+
+      it 're renders new' do
+        expect(request).to render_template :new
+      end
+
+      it { should set_flash }
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should belong_to(:creator).class_name("User") }
   it { should have_many(:answers) }
+  it { should have_and_belong_to_many(:assessments) }
 
   it { should validate_presence_of(:prompt) }
 end

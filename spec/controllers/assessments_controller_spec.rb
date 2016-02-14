53ef148fdbd2f3d78e79c37010d3960d909dceb9
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-02-14 17:42:00 -0600

    Test assigning questions to assessment

--- spec/controllers/assessments_controller_spec.rb
+++ spec/controllers/assessments_controller_spec.rb
@@ -45,4 +45,47 @@ RSpec.describe AssessmentsController, type: :controller do
       it { should set_flash }
     end
   end
+
+  describe 'get SHOW' do
+    let(:assessment) { FactoryGirl.create(:assessment, :with_questions) }
+
+    before do
+      expect(assessment.questions).to_not be_empty
+      request = get :show, { id: assessment.id }
+    end
+
+    it 'renders proper show' do
+      expect(request).to render_template :show
+    end
+  end
+
+  describe 'get ADD_QUESTIONS' do
+    let(:assessment) { FactoryGirl.create(:assessment) }
+
+    before do
+      get :add_questions, { assessment_id: assessment.id }
+    end
+
+    it 'renders proper page' do
+      expect(request).to render_template :add_questions
+    end
+  end
+
+  describe 'put ASSIGN_QUESTIONS' do
+    let(:first_question) { FactoryGirl.create(:question) }
+    let(:second_question) { FactoryGirl.create(:question) }
+    let(:assessment) { FactoryGirl.create(:assessment) }
+    let(:question_ids) { [first_question.id, second_question.id] }
+
+    before do 
+      request = put :assign_questions, 
+      { assessment_id: assessment.id, 
+        assessment: { question_ids: question_ids } }
+    end
+
+    it 'assigns the questions to the assessment' do
+      expect(assessment.questions).to include(first_question)
+      expect(assessment.questions).to include(second_question)
+    end
+  end
 end
